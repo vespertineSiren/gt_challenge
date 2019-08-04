97848652ab9b23285780ba58ad70ee85af0031cb
Patrick Martin <patrickmartin88@gmail.com>
2019-08-04 13:51:22 -0700

    initial

---
+++ app/src/main/java/dev/vespertine/myapplication/PinData.kt
@@ -0,0 +1,9 @@
+package dev.vespertine.myapplication
+
+data class PinData(
+    val description: String,
+    val id: Int,
+    val latitude: Double,
+    val longitude: Double,
+    val name: String
+)
