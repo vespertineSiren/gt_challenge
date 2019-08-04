488c12affe5c0e6662fd15090427c8ea81d3520e
Patrick Martin <patrickmartin88@gmail.com>
2019-08-04 15:02:07 -0700

    Added MainComponent and AppModule. Initial before updating.

---
+++ app/src/main/java/dev/vespertine/myapplication/di/module/AppModule.kt
@@ -0,0 +1,16 @@
+package dev.vespertine.myapplication.di.module
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class AppModule(private val context: Context) {
+
+    @Provides
+    @Singleton
+    fun provideAppContext(): Context {
+        return context.applicationContext
+    }
+}
